0c04f07e471d7a702276905124c5a370f225a5e2
Matt <mjheller414@gmail.com>
2015-12-07 18:14:39 -0600

    first commit, finished HappyNumber Problem

---
+++ devCodeCampExtendedMathTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using devCodeCampExtendedMath;
+
+namespace devCodeCampExtendedMathTest
+{
+    class Math
+    {
+
+        static void Main(string[] args)
+        {
+            int index = 1;
+            List<int> happyNumberList;
+            happyNumbers happyNumbers = new happyNumbers();
+            happyNumberList = happyNumbers.toNthHappyNumber(2000);
+            foreach(int i in happyNumberList)
+            {
+                Console.WriteLine("HappyNumber " + i + " -- Count  " + index);
+                index++;
+            }
+            string keepopen = Console.ReadLine();
+        }
+    }
+}
+
---
+++ happyNumbers/happyNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace devCodeCampExtendedMath
+{
+    public class happyNumbers
+    {
+        List<int> Duplicates;
+        List<int> squaredDigits;
+        
+        public happyNumbers()
+        {
+            Duplicates = new List<int>();
+            squaredDigits = new List<int>();
+        }
+        public bool isHappy(int number)
+        {
+            int num = number;
+            
+
+            while (num != 1)
+            {
+                if (Duplicates.Contains(num))
+                {
+                    return false;
+                }
+                Duplicates.Add(num);
+                
+
+                string digitString = num.ToString();
+                char[] digits = digitString.ToCharArray();
+
+                foreach (int digit in digits)
+                {
+                    int result = digit * digit;
+                    squaredDigits.Add(result);
+                }
+                num = squaredDigits.Sum();
+                squaredDigits.Clear();
+            }
+            return true;
+        }
+    }
+}
